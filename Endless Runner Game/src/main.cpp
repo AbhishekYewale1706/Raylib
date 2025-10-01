5894532df63e8c7f43da0659d67a768c804134df
Abhishek Yewale <1706abhishekyewale@gmail.com>
2025-10-01 12:57:53 +0530

    update

--- Endless Runner Game/src/main.cpp
+++ Endless Runner Game/src/main.cpp
@@ -1,3 +1,35 @@
-
-#include<iostream>
 #include<raylib.h>
+
+int main()
+{
+    const int ScreenWidth=1200;
+    const int ScreenHeight=700;
+    InitWindow(ScreenWidth,ScreenHeight,"EndlessRunner");
+    SetTargetFPS(60);
+
+    //Rectange Dimension
+    const int RecWidth{50};
+    const int RecHeight{80};
+    int posY{GetScreenHeight()-RecHeight};
+    int Velocity{0};
+
+    //Main Loop
+    while (WindowShouldClose()==false)
+    {
+        BeginDrawing();
+        ClearBackground(GREEN);
+        
+        if(IsKeyPressed(KEY_SPACE))
+        {
+            Velocity-=10;
+        }
+        posY+=Velocity;
+
+        DrawRectangle(GetScreenWidth()/2,posY,RecWidth,RecHeight,BLUE);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
+}
--- Snake Game/src/main.cpp
+++ Snake Game/src/main.cpp
@@ -1,53 +1,88 @@
+#include <raylib.h>
+#include <deque>
+#include <cstdlib>
+#include <ctime>
 
-#include<iostream>
-#include<raylib.h>
+const int screenWidth = 800;
+const int screenHeight = 600;
+const int cellSize = 20;
+const int cols = screenWidth / cellSize;
+const int rows = screenHeight / cellSize;
 
-const int CellSize=30;
-const int CellCount=25;
-
-class Food
-{
-public:
-        Vector2 Position={5,6};
-        Texture2D texture;
-
-        Food()//Constructor
-        {
-            Image image=LoadImage("Image/food.png");
-            texture=LoadTextureFromImage(image);
-            UnloadImage(image);
-        }
-        ~Food()//Destructor
-        {
-            UnloadTexture(texture);
-        }
-        void Draw()
-        {
-           DrawTexture(texture,Position.x*CellSize,Position.y*CellSize,WHITE);
-        }
-
+struct Segment {
+    int x, y;
 };
 
-int main()
-{
+std::deque<Segment> snake;
+Segment food;
+int dirX = 1, dirY = 0;
+bool gameOver = false;
+
+// Reset game 
+void ResetGame() {
+    snake.clear();
+    snake.push_back({cols / 2, rows / 2});
+    dirX = 1; dirY = 0;
+    food = { rand() % cols, rand() % rows };
+    gameOver = false;
+}
+
+
+void UpdateSnake() {
     
-    //Window Setup
-    InitWindow(CellSize*CellCount,CellSize*CellCount,"Snake Game");
-    SetTargetFPS(60);
-
-    Food food=Food();
-
-    while (WindowShouldClose()==false)
-    {
-        BeginDrawing();
-        ClearBackground(GREEN);
-        //Event Handling
-        //Updated
-        //Draw
-        food.Draw();
-        EndDrawing();
+    Segment newHead = { snake.front().x + dirX, snake.front().y + dirY };
+    if (newHead.x < 0) newHead.x = cols - 1;
+    if (newHead.x >= cols) newHead.x = 0;
+    if (newHead.y < 0) newHead.y = rows - 1;
+    if (newHead.y >= rows) newHead.y = 0;
+    for (auto &seg : snake) {
+        if (seg.x == newHead.x && seg.y == newHead.y) {
+            gameOver = true;
+            return;
+        }
     }
-  
+
+    snake.push_front(newHead);
+    if (newHead.x == food.x && newHead.y == food.y) {
+        food = { rand() % cols, rand() % rows }; 
+    } else {
+        snake.pop_back(); 
+    }
+}
+
+
+int main() {
+    InitWindow(screenWidth, screenHeight, "Snake Game - Raylib");
+    SetTargetFPS(10); 
+
+    srand(time(0));
+    ResetGame();
+
+    while (!WindowShouldClose()) {
+        // Controls
+        if (IsKeyDown(KEY_W) && dirY != 1) { dirX = 0; dirY = -1; }
+        if (IsKeyDown(KEY_S) && dirY != -1) { dirX = 0; dirY = 1; }
+        if (IsKeyDown(KEY_A) && dirX != 1) { dirX = -1; dirY = 0; }
+        if (IsKeyDown(KEY_D) && dirX != -1) { dirX = 1; dirY = 0; }
+
+        if (!gameOver) {
+            UpdateSnake();
+        } else if (IsKeyPressed(KEY_ENTER)) {
+            ResetGame();
+        }
+
+        BeginDrawing();
+        ClearBackground(BLACK);
+        DrawRectangle(food.x * cellSize, food.y * cellSize, cellSize, cellSize, RED);
+        for (auto &seg : snake) {
+            DrawRectangle(seg.x * cellSize, seg.y * cellSize, cellSize, cellSize, GREEN);
+        }
+        if (gameOver) {
+            DrawText("GAME OVER! Press Enter to Restart", screenWidth/2 - 160, screenHeight/2, 20, WHITE);
+        }
+            EndDrawing();
+        }
+
     CloseWindow();
     return 0;
 }
